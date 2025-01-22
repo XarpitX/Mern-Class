9bdbb8195c5b8fcf4edcc8d1c3cad4cc7893fefa
Xaarpit <arpittak02@gmail.com>
2025-01-22 16:08:00 +0530

    JAVASCRIPT

---
+++ Day7(JS)/JS1.js
@@ -0,0 +1,17 @@
+console.log("Hello ARPIT");
+
+
+const ans1 = 10 - 5;
+console.log(ans1);
+
+const ans2 ="HELLO" - 5;
+console.log(ans2);
+
+
+const ans3 = "HELLO" - "ALL";
+console.log(ans3);
+
+
+const ans4 = 10 - "HELLO";
+console.log(ans4);
+
---
+++ Day7(JS)/JS2.js
@@ -0,0 +1,18 @@
+var city = "Delhi";
+console.log(city);
+console.log(country);
+var country = "INDIA";
+
+city = "India";
+console.log(city);
+
+
+
+let citi = "Mumbai";
+console.log(citi);
+
+console.log(state);
+let state = "MAHARASTRA";
+
+
+
